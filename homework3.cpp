7bd66bee5f8908787dd87d9ab73f6850d4b98d67
DmitriyKor <dk@das-ua.com>
2024-08-21 21:35:37 +0300

    homework 3

---
+++ homework3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  /*
+  Мій Вік
+  Площа Заднього Двору
+  Зірок У Галактиці
+  Середньомісячний Рівень Опадів
+  */
+  int MyAge;
+  double BackyardArea;
+  long long StarQty;
+  double AveragePrecipitationMonthly;
+
+  const double Pi = 3.14159;
+
+  float MyValue = Pi; 
+
+
+  return 0;
+} 
