548d758ec6219ad02b925c596fdd054be99680e5
DmitriyKor <dk@das-ua.com>
2024-10-02 19:51:48 +0300

    12

--- homework11.cpp
+++ homework11.cpp
@@ -17,6 +17,9 @@ protected:
     int mWidth;
 public:
     Rectangle(int length, int width){mLength=length; mWidth=width;};
+    virtual Rectangle* Clone() {return new Rectangle(this->mLength, this->mWidth);};
+    virtual ~Rectangle() {};
+    void ShowSize() {cout<<mLength <<' ' <<mWidth << "\n"; }
 };
 
 
@@ -24,10 +27,26 @@ class Square : public Rectangle
 {
 private:
 public:
-    Square(int length) : Rectangle(length, length){}
+    Square (int length) : Rectangle(length, length){}
+    virtual Square* Clone() {return new Square(this->mLength);};
 };
+
+
+
 
 int main()
 {
+    Rectangle *s1, *s2;
+
+    s1=new Square(5);
+    s2=s1->Clone();
+
+    s1->ShowSize();
+    s2->ShowSize();
+
+    
+    delete s1;
+    delete s2;
+
     return 0;
 }
