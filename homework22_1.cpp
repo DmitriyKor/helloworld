88475c54bf3a8e3c419cbdb6ea73b7e657689892
DmitriyKor <dk@das-ua.com>
2024-11-24 17:48:28 +0200

    22

--- homework22_1.cpp
+++ homework22_1.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 template <typename T>
 class List;
 
@@ -10,58 +9,51 @@ template <typename T>
 class ListCell
 {
 public:
-   	friend class List<T>;
-    ListCell(T value, ListCell *cell = 0) : val(value),	next(cell) {}
-   	T val;
-   	ListCell *next;
+    friend class List<T>;
+    ListCell(T value, ListCell *cell = 0) : val(value), next(cell) {}
+    T val;
+    ListCell *next;
 };
-
-
-template <typename T>
-class List;
-
-//template <typename U> bool operator==(const List<U> & rList1, const List<U> & rList2);
-
 
 template <typename T>
 class List
 {
 public:
-	List() : head(0), tail(0), theCount(0) {}
-	virtual ~List();
-	void insert(T value);
-	void append(T value);
-	int is_present(T value) const;
-	int is_empty() const
-	{
-    	return head == 0;
-	}
-	int count() const { return theCount; }
+    List() : head(0), tail(0), theCount(0) {}
+    virtual ~List();
+    void insert(T value);
+    void append(T value);
+    int is_present(T value) const;
+    int is_empty() const
+    {
+        return head == 0;
+    }
+    int count() const { return theCount; }
     void display() const;
     template <typename U>
-    friend bool operator==(const List<U> & rList1, const List<U> & rList2);
+    friend bool operator==(const List<U> &rList1, const List<U> &rList2);
+
 private:
-	T val;
-	ListCell<T> *next;
-	ListCell<T> *head;
-	ListCell<T> *tail;
-	int theCount;
+    T val;
+    ListCell<T> *next;
+    ListCell<T> *head;
+    ListCell<T> *tail;
+    int theCount;
 };
-
-
 
 class Cat
 {
 public:
-    Cat() : mWeight(0){};
+    Cat() : mWeight(0) {};
     Cat(float weight) : mWeight(weight) {};
-    ~Cat(){};
-    friend ostream& operator<<(ostream &os, const Cat & rCat);
+    ~Cat() {};
+    friend ostream &operator<<(ostream &os, const Cat &rCat);
+
 private:
     float mWeight;
 };
 
-ostream& operator<<(ostream &os, const Cat & rCat)
+ostream &operator<<(ostream &os, const Cat &rCat)
 {
     os << "weight " << rCat.mWeight;
     return os;
@@ -70,91 +62,88 @@ ostream& operator<<(ostream &os, const Cat & rCat)
 template <typename U>
 bool operator==(const List<U> &rList1, const List<U> &rList2)
 {
-    if (rList1.theCount!=rList2.theCount) 
+    if (rList1.theCount != rList2.theCount)
         return false;
 
-    if (rList1.theCount==0)
+    if (rList1.theCount == 0)
         return true;
 
-    ListCell<U> *cell1 = rList1.head;    
-    ListCell<U> *cell2 = rList2.head;    
+    ListCell<U> *cell1 = rList1.head;
+    ListCell<U> *cell2 = rList2.head;
 
-    while (cell1!=0)
+    while (cell1 != 0)
     {
-        if (cell1->val!=cell2->val) 
+        if (cell1->val != cell2->val)
         {
             return false;
         }
         cell1 = cell1->next;
         cell2 = cell2->next;
-    }  
+    }
     return true;
 }
-
-
-
 
 template <typename T>
 void List<T>::insert(T value)
 {
-    head = new ListCell (value, head);
+    head = new ListCell(value, head);
     theCount++;
-    if (theCount==1)
+    if (theCount == 1)
         tail = head;
 }
 
 template <typename T>
 void List<T>::append(T value)
 {
-    if (tail==0)
+    if (tail == 0)
     {
         tail = new ListCell<T>(value, 0);
         head = tail;
-    }   
-    else 
+    }
+    else
     {
-        tail->next = new ListCell<T>(value, 0); 
-        tail = tail->next; 
-    }; 
+        tail->next = new ListCell<T>(value, 0);
+        tail = tail->next;
+    };
     theCount++;
 }
 
 template <typename T>
 int List<T>::is_present(T value) const
 {
-    ListCell<T> *cell = head;    
-    //cout << "\nValue is "<<value << endl;
-    while (cell!=0)
+    ListCell<T> *cell = head;
+    // cout << "\nValue is "<<value << endl;
+    while (cell != 0)
     {
-        if (cell->val==value) 
+        if (cell->val == value)
         {
-            //cout << "\nCell value is "<<cell->val <<endl;
+            // cout << "\nCell value is "<<cell->val <<endl;
             return 1;
         }
         cell = cell->next;
-    }  
-    return 0;  
+    }
+    return 0;
 }
 
 template <typename T>
 void List<T>::display() const
 {
-    //cout << "type of the list: " << typeid(T).name() << endl;
+    // cout << "type of the list: " << typeid(T).name() << endl;
     cout << "\ncontent of the list:\n";
-    ListCell<T> *cell = head;    
-    while (cell!=0)
+    ListCell<T> *cell = head;
+    while (cell != 0)
     {
-        cout << cell->val <<" -> ";
+        cout << cell->val << " -> ";
         cell = cell->next;
-    }  
+    }
     cout << endl;
 }
 template <typename T>
 List<T>::~List()
 {
     ListCell<T> *cell;
-    next = head;    
-    while (next!=0)
+    next = head;
+    while (next != 0)
     {
         cell = next;
         next = next->next;
@@ -176,48 +165,45 @@ void swapvalues(T &val1, T &val2)
 
 int main()
 {
-    List<string> * MyStringList = new List<string>;
+    List<string> *MyStringList = new List<string>;
     MyStringList->insert("Hello");
     MyStringList->append("Very");
     MyStringList->insert("Low");
-    MyStringList->display();  
+    MyStringList->display();
     cout << "'High' is " << (MyStringList->is_present("High") ? "" : "not ") << "present\n";
     cout << "'Low' is " << (MyStringList->is_present("Low") ? "" : "not ") << "present\n";
 
-
-
-    List<int> * MyIntList = new List<int>;
+    List<int> *MyIntList = new List<int>;
     MyIntList->insert(10);
     MyIntList->insert(8);
     MyIntList->append(20);
     MyIntList->insert(3);
-    MyIntList->display();   
-    
+    MyIntList->display();
+
     Cat *MyCat;
-    List<Cat> * MyCatList = new List<Cat>;
+    List<Cat> *MyCatList = new List<Cat>;
     MyCat = new Cat(3.3);
     MyCatList->insert(*MyCat);
     MyCat = new Cat(5.3);
     MyCatList->insert(*MyCat);
     MyCatList->display();
 
-
-    List<int> * MyIntList2 = new List<int>;
+    List<int> *MyIntList2 = new List<int>;
     MyIntList2->insert(10);
     MyIntList2->insert(8);
     MyIntList2->append(20);
-    MyIntList2->insert(/*3*/2);
+    MyIntList2->insert(/*3*/ 2);
     bool equals = (*MyIntList == *MyIntList2);
     cout << "Two list check is " << equals << endl;
 
     delete MyStringList;
     delete MyIntList;
     delete MyCatList;
-    
+
     int a1 = 45;
     int a2 = 65;
     swapvalues(a1, a2);
     cout << "\n swap test : " << a1 << ", " << a2 << endl;
-    
+
     return 0;
 }
